af47af901138c0c7a054eba6568a317f33e62fce
daw1 - Varela Docampo <A24ChristianVD@sanclemente.local>
2025-02-10 16:06:29 +0100

    t04_02 engadido + zip

--- Estruturas de datos/t04_02_Varela_Docampo/exer5/App.java
+++ Estruturas de datos/t04_02_Varela_Docampo/exer5/App.java
@@ -1,5 +1,6 @@
 package exer5;
 
+// Todos os importes necesarios
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -9,39 +10,65 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App{
+    // As ArrayList defínoas fora de main porque en principio facíanme falta pa usalas nun método, pero cambieino e como tal non
+    // faría falta que estean aquí, igualmente creo que qeuda máis bonito que deixalas en main
     public static ArrayList<String> nomes = new ArrayList<>();
     public static ArrayList<LocalDateTime> citas = new ArrayList<>();
+
+    // Os formatos si que os deixo fora de main xa que os emprego no método, que está fora de main
     public static final DateTimeFormatter formatoEngadirCita = DateTimeFormatter.ofPattern("HH:mm d/M/yyyy");
     public static final DateTimeFormatter formatoEliminarCita = DateTimeFormatter.ofPattern("d/M/yyyy HH:mm");
     public static final DateTimeFormatter formatoSeleccionarCita = DateTimeFormatter.ofPattern("d-M-yyyy");
+
+    // Esto está aquí pq algúns dos meu plugins fan que o scanner apareza amarillo se non lle poño tries, unha pijada
     @SuppressWarnings({ "ConvertToTryWithResources" })
     public static void main(String[] Args){
-        LocalDateTime cita = LocalDateTime.parse("12:30 10/2/2025", formatoEngadirCita);
         boolean programa = true;
+
+        // Este bloque todo é so para engadir os datos ó programa cando se inicie
+        LocalDateTime cita = LocalDateTime.parse("12:30 10/2/2025", formatoEngadirCita);
         for(int i = 0; i<4; i++){
             nomes.add("Nome"+i);
             cita = cita.plus(1, ChronoUnit.DAYS);
             citas.add(cita);
         }
+
+        // O while principal
         while(programa == true){
+            // Defino o scanner aquí para que se reinicie o buffer cada repetición
             Scanner scanner = new Scanner(System.in);
             System.out.println("Qué desexa facer?\n\ta) Engadir cita\n\tb) Eliminar cita\n\tc) Seleccionar data\n\td) Sair");
-            String opc = scanner.nextLine();
+            char opc = scanner.nextLine().charAt(0);
+
             switch (opc) {
-                case "a" -> {
+
+                // Ingresar unha nova cita
+                case 'a' -> {
                     System.out.println("Introduza o nome do paciente: ");
                     String new_nome = scanner.nextLine();
                     System.out.println("Introduza a fecha da cita (HH:mm d/M/yyyy):");
                     String new_cita_raw = scanner.nextLine();
+
+                    // O de definir os formatos de antemán fora de main mola moito
                     LocalDateTime new_cita = LocalDateTime.parse(new_cita_raw, formatoEngadirCita);
-                    Boolean cita_invalida = false;
+
+                    // boolean para comprobar se a cita é válida
+                    boolean cita_invalida = false;
+
+                    // Comprobamos que o día da semana non sexa sábado ou domingo
                     if(new_cita.getDayOfWeek() == DayOfWeek.SUNDAY || new_cita.getDayOfWeek() == DayOfWeek.SATURDAY){
                         System.out.println("A cita non pode ser un sábado ou domingo.");
                         cita_invalida = true;
                     }
+
+                    // Comprobamos que a cita introducida non sexa media hora despóis doutra ou media hora antes
+                    // Imos cita por cita comprobando cada unha
                     for(LocalDateTime citaCheck:citas){
+                        // Primeiro comprobamos se son do mesmo ano, mes e día, nese orde
                         if(citaCheck.getYear() == new_cita.getYear() || citaCheck.getMonth() == new_cita.getMonth() ||
                                 citaCheck.getDayOfMonth() == new_cita.getDayOfMonth()){
+
+                            // A continuación comprobamos que a cita non sexa media hora despóis doutra ou media hora antes
                             if((cita.isBefore(new_cita.plus(30, ChronoUnit.MINUTES)) && cita.isAfter(new_cita))
                              ||(cita.isAfter(new_cita.minus(30, ChronoUnit.MINUTES)) && cita.isBefore(new_cita))){
                                 System.out.println("Xa hai citas a esa hora.");
@@ -50,45 +77,70 @@ public class App{
                             }
                         }
                     }
+                    // Se a cita é inválida mandamos o usuario ó menu
                     if(cita_invalida == true) continue;
+
+                    // Se é válida, gardamos os valores nas ArrayLists
                     nomes.add(new_nome);
                     citas.add(new_cita);
                     System.out.println("Cita engadida.");
                 }
-                case "b" -> {
+
+                // Eliminar unha cita existente
+                case 'b' -> {
+
+                    // Mostramos todalas citas xunto co seu índice
                     System.out.println(mostrarCitas(nomes, citas, formatoEliminarCita));
                     System.out.println("Seleccione a cita a eliminar");
                     int selec = scanner.nextInt();
+
+                    // Comprobamos que o índice sexa válido
                     if(selec <= 0 || selec > citas.size()){
                         System.out.println("Índice de cita non existente.");
                         continue;
                     }
+
+                    // Se é válido eliminamos a cita seleccionada
                     nomes.remove(selec-1);
                     citas.remove(selec-1);
                     System.out.println("Cita eliminada.");
                     break;
                 }
-                case "c" -> {
+
+                // Mostrar citas dun día
+                case 'c' -> {
+
+                    // Creamos novas ArrayLists para as citas do día introducido
                     ArrayList<String> nomesC = new ArrayList<>();
                     ArrayList<LocalDateTime> citasC = new ArrayList<>();
+
                     System.out.println("Introduza a data (dd-MM-yyyy)");
                     String data_raw = scanner.nextLine();
                     LocalDate data = LocalDate.parse(data_raw, formatoSeleccionarCita);
+
+                    // Imos cita por cita
                     for(LocalDateTime citaCheck:citas){
+                        // Comprobamos se a cita que tamos comprobando está no mesmo día que a data introducida
                         if(data.getYear() == citaCheck.getYear() && data.getMonth() == citaCheck.getMonth() &&
                         data.getDayOfMonth() == citaCheck.getDayOfMonth()){
+                            // Se é así, engadimos a cita ás novas ArrayList que creamos
                             citasC.add(citaCheck);
                             nomesC.add(nomes.get(citas.indexOf(citaCheck)));
                         }
                     }
+                    // Mostramos as novas ArrayLists
                     System.out.println(mostrarCitas(nomesC, citasC, formatoSeleccionarCita));
                 }
-                case "d" -> {
+
+                // Sair do programa
+                case 'd' -> {
                     System.out.println("Saindo do programa...");
                     programa = false;
                     scanner.close();
                     break;
                 }
+
+                // Se algunha das opións non é válida
                 default -> {
                     System.out.println("Opción inválida");
                     continue;
@@ -97,6 +149,13 @@ public class App{
         }
     }
 
+    /**
+     * Método para mostrar as citas, necesita dous arraylists introducidos ó chamar ó método e un formato que se introduce por defecto
+     * @param nomes Lista dos nomes
+     * @param citas Lista das citas
+     * @param formato Formato co que queremos mostrar a información, está aquí xa que en diferentes opción queremos mostrar diferentes formatos
+     * @return Un String codificada con toda a información
+     */
     public static String mostrarCitas(ArrayList<String> nomes, ArrayList<LocalDateTime> citas, DateTimeFormatter formato){
         String citasString = "";
         for(int i = 0; i < nomes.size(); i++){
---
+++ Estruturas de datos/t04_02_Varela_Docampo/exer6/App.java
@@ -0,0 +1,153 @@
+package exer6;
+
+// Todos os imports
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class App{
+    
+    // Como tal non faría falta ter aquí os formatos de datetime pero apeteceume pq queda ben
+    public static final DateTimeFormatter formatoFichadoFull = DateTimeFormatter.ofPattern("HH:mm d/M/yyyy");
+    public static final int ano = LocalDateTime.now().getYear();
+
+    public static void main(String[] Args){
+
+        // Definimos os hash maps e arraylists
+        HashMap<String, ArrayList<LocalDateTime>> mapa = new HashMap<>();
+        ArrayList<LocalDateTime> ficha = new ArrayList<>();
+
+        // Todo este bloque é para engadir os valores, ignoralo
+        mapa.put("17929701M", ficha);
+        mapa.put("56306071R", ficha);
+        mapa.put("66188536X", ficha);
+        for (Map.Entry<String, ArrayList<LocalDateTime>> entry : mapa.entrySet()) {
+            if(!checkDni(entry.getKey())){
+                System.out.println("DNI inválido!");
+                continue;
+            }
+            ArrayList<LocalDateTime> tempFicha = new ArrayList<>();
+            LocalDateTime novaFecha = LocalDateTime.now();
+            for(int i = 0; i < mapa.size()+2; i++){
+                tempFicha.add(novaFecha);
+                novaFecha = novaFecha.plus(2, ChronoUnit.HOURS);
+            }
+            mapa.put(entry.getKey(), tempFicha);
+            for(int i = 0; i < entry.getValue().size(); i++){
+                System.out.println("Clave: " + entry.getKey() + ", Valor: " + entry.getValue().get(i).format(formatoFichadoFull));
+            }
+            System.out.println();
+        }
+
+        // boolean para comporbar que o loop siga
+        boolean programa = true;
+
+        while(programa == true){
+
+            // Inicializamos o scanner e pedimos ó usuario que facer
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Que desexa facer?\n\ta) Fichar entrada/saida\n\tb) Comprobar salario\n\tc) Sair");
+            char opc = scanner.nextLine().charAt(0);
+
+            switch (opc) {
+
+                // Engadir fichado
+                case 'a' -> {
+
+                    // Pedimos o DNI
+                    System.out.println("Introduza seu DNI: ");
+                    String dniAComprobar = scanner.nextLine();
+
+                    // Comprobamos o DNI
+                    if(!checkDni(dniAComprobar) || !mapa.containsKey(dniAComprobar)){
+                        System.out.println("DNI inválido.");
+                        break;
+                    }
+
+                    // Pedimos e comprobamos a data de fichado
+                    System.out.println("Introduza a hora de fichado (HH:mm d/M)");
+                    // Como tal a data pídese en formato (HH:mm d/M) pero fai falta que teña o ano para poder facer o parse polo que engádoo logo
+                    String horaAComprobar = scanner.nextLine()+"/"+ano;
+                    LocalDateTime novaFecha = LocalDateTime.parse(horaAComprobar, formatoFichadoFull);
+                    mapa.get(dniAComprobar).add(novaFecha);
+                    System.out.println("Fichado correctamente.");
+                }
+
+                // Comprobar soldo
+                case 'b' -> {
+
+                    // Pedimos e comprobamos o DNI
+                    System.out.println("Introduza seu DNI: ");
+                    String dniAComprobar = scanner.nextLine();
+                    if(!checkDni(dniAComprobar) || !mapa.containsKey(dniAComprobar)){
+                        System.out.println("DNI inválido.");
+                        break;
+                    }
+
+                    // Definimos todas as variables necesarias para facer as comprobacións e a suma das horas traballadas
+                    Duration traballoTotal = Duration.ZERO;
+                    LocalDateTime horaEntrada = null;
+                    LocalDateTime horaSaida = null;
+                    Duration traballado = null;
+                    // Lista dsa horas de entrada e saida do DNI seleccionado
+                    ArrayList<LocalDateTime> listaFichados = mapa.get(dniAComprobar);
+
+                    // Imos valor por valor, tendo en conta que os pares son as horas de entrada e os impares as de saida
+                    for(int i = 0; i < listaFichados.size(); i++){
+                        // As de entrada son os pares e mais 0, que non sei sei pasa o check ó facer (0 % 2 =?= 0)
+                        if(i == 0 || i % 2 == 0) horaEntrada = listaFichados.get(i); // Se é par, gardamos a hora como hroa de entrada
+                        // Se non é par, gardamos a hroa coma hora de saida e calculamos o tempo traballado nese intervalo
+                        else{
+                            horaSaida = listaFichados.get(i);
+                            traballado = Duration.between(horaEntrada, horaSaida);
+                            // Sumamos o intervalo calculado ó total de hroas traballadas
+                            traballoTotal = traballoTotal.plus(traballado);
+                        }
+                    }
+
+                    // Pasamos o intervalo total a minutos e calculamos o salario
+                    double salario = traballoTotal.toMinutes() * 12/60;
+                    System.out.println("O seu salario é de "+salario+" euros.");
+                }
+
+                // Pechar programa
+                case 'c' -> {
+                    System.out.println("Pechando programa...");
+                    scanner.close();
+                    programa = false;
+                }
+                
+                // Se o usuario introduce unha opción inválida...
+                default -> {
+                    System.out.println("Opción inválida, tente de novo.");
+                }
+            }
+        }
+    }
+
+    /**
+     * Método que comproba se un DNI é válido ou non
+     * @param DNI DNI a comprobar, String
+     * @return true se o DNI é válido, false se non
+     */
+    public static boolean checkDni(String DNI){
+        // Comprobamos que a lonxitude sexa correcta
+        if (DNI.length() != 9) return false;
+        
+        // Creamos a lista das letras para a comprobación
+        char[] letras = {'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'};
+
+        // Separamos o DNI en números e letras
+        int numeros = Integer.parseInt(DNI.substring(0,8));
+        char letra = DNI.charAt(8);
+
+        // Comrpobamos que a letra introducida é a corretcta para o número do DNI
+        int resto = numeros % 23;
+        return (letras[resto] == letra);
+    }
+}
