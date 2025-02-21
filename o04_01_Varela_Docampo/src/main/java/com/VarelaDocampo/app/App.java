2f6a6fb25a0c17744340988f14fcd9754ea279df
daw1 - Varela Docampo <A24ChristianVD@sanclemente.local>
2025-02-21 12:58:08 +0100

    fixed

--- o04_01_Varela_Docampo/src/main/java/com/VarelaDocampo/app/App.java
+++ o04_01_Varela_Docampo/src/main/java/com/VarelaDocampo/app/App.java
@@ -101,6 +101,7 @@ public class App {
                                 continue;
                             }
                         }
+                        oMapper.writeValue(jsonFile, config);
                         break;
                     }
                 }
