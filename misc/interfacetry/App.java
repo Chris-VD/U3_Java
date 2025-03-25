81599ea0c493820b564cd6e1e99841bc16f48d1d
Infe <infe@infe.home>
2025-03-25 23:59:03 +0100

    mic added

---
+++ misc/interfacetry/App.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class App{  
+    public static void main(String[] args){
+        CocheE electrico = new CocheE();
+        CocheG gasolina = new CocheG();
+        ArrayList<Coche> coches = new ArrayList<>();
+        coches.add(electrico);
+        coches.add(gasolina);
+
+        for (Coche c:coches){
+            if (c instanceof ICocheG){
+                ((CocheG)c).gasolina();
+            }
+            else System.out.println("Non son gasolina");
+        }
+    }
+}
